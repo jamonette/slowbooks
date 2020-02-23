826e28d8e7264333d00c08702feff15cd7dd3720
Jake Amonette <idem@jamonette.com>
2020-02-23 14:06:45 -0800

    Auto-generate closing entries, cleanup core API

--- core/data.py
+++ core/data.py
@@ -1,4 +1,3 @@
-import functools
 import numpy as np
 import pandas as pd
 
@@ -13,127 +12,124 @@ DUMMY_TRANSACTION_ID = -1
 #### Raw data #################################################################
 ###############################################################################
 
-def chart_of_accounts(data_dir):
+def fetch_chart_of_accounts(data_dir):
     raw_coa = pd.read_csv(data_dir / CHART_OF_ACCOUNTS_PATH)
-
     cat_cols = (raw_coa['category']
                 .reset_index(drop=True)
                 .str.split(':', expand=True)
                 .fillna(axis=1, method='pad')
-                .pipe(lambda df: df.rename(lambda col_num: f'category_{col_num}', axis=1)))
+                .rename(lambda col_num: f'category_{col_num}', axis=1))
 
-    return (raw_coa.join(cat_cols)
-                   .drop(columns=['category']))
+    return (raw_coa
+            .join(cat_cols)
+            .drop(columns=['category'])
+            .rename(columns={'id': 'account_id', 'name': 'account_name'}))
 
-def master_journal(data_dir, coa):
+def fetch_master_journal(data_dir, chart_of_accounts):
     mj_file = data_dir / MASTER_JOURNAL_PATH
     mj = (pd.read_csv(mj_file)
           .astype({'date': 'datetime64[ns]'})
           .rename(columns={'id': 'transaction_id'}))
 
-    return enrich_journal(coa, mj)
+    return _build_journal(chart_of_accounts, mj)
 
-def balance_data(data_dir, period_range):
+def fetch_balance_data(data_dir):
     files = [f for f in (data_dir / BALANCE_DATA_DIR).glob('**/*') if f.is_file()]
     return pd.concat([pd.read_csv(f)
                         .astype({'date': 'datetime64[ns]'})
-                        .assign(period=lambda df: df['date'].dt.to_period(freq=period_range.freqstr))
-                        .groupby(['account_name', 'period'])
-                        .apply(lambda df: df.sort_index()['balance'].head(1))
-                        .to_frame()
-                        .reset_index()
-                        .set_index(['account_name', 'period'])
                      for f in files])
 
 ###############################################################################
-#### Enrich + modify #################################################################
+#### Core DataFrame shapes ####################################################
 ###############################################################################
 
-def enrich_journal(chart_of_accounts, raw_journal, join_key='account_id'):
-    def net_amount_predicate(df):
-        return (((df['action'] == 'debit') & (df['debit_increases_balance'] == True)) |
-                ((df['action'] == 'credit') & (df['debit_increases_balance'] == False)))
+# given a journal, add filler data such that there is at least one
+# (blank) line item for each (account, period) combination in all
+# downstream reports
 
-    return (raw_journal.merge(right=chart_of_accounts,
-                              how='left',
-                              left_on=join_key,
-                              right_on='id' if join_key == 'account_id' else 'name',
-                              validate='many_to_one',
-                              suffixes=(False, False))
-            .assign(account_name=lambda df: df['name'])
-            .assign(account_id=lambda df: df['id'])
-            .assign(net_amount=lambda df: np.where(net_amount_predicate(df), df['amount'], df['amount'] * -1))
+def statement_data(chart_of_accounts,
+                   journal,
+                   period_range,
+                   with_gains=False,
+                   balance_data=None):
+
+    statement =\
+        (chart_of_accounts
+         # generate all combinations of (account, period) by faking
+         # a cartesian join using a special key
+         .assign(crossjoin_key=1)
+         .merge(right=(period_range
+                       .to_frame(name='period')
+                       .assign(crossjoin_key=1)),
+                on='crossjoin_key')
+         .drop('crossjoin_key', 1)
+         .rename(columns={'id': 'account_id', 'name': 'account_name'})
+         .assign(date=lambda df: df['period'].dt.start_time)
+         .assign(credit_amount=0.0)
+         .assign(debit_amount=0.0)
+         .assign(net_amount=0.0)
+         .assign(description='')
+         .assign(action=None)
+         .assign(transaction_id=DUMMY_TRANSACTION_ID)
+         .pipe(lambda df: df[_get_statement_columns(df)])
+
+         # combine with the actual journal data
+         .append(other=_journal_to_statement(journal, period_range), sort=False)
+         .pipe(lambda df: _journal_to_statement(df, period_range)))
+
+    generated_entries = [
+        _generate_inferred_gains(chart_of_accounts, statement, balance_data, period_range) if with_gains else pd.DataFrame(),
+        _generate_closing_entries(chart_of_accounts, statement, period_range),
+    ]
+
+    return (pd
+            .concat(generated_entries)
+            .pipe(lambda df: _journal_to_statement(df, period_range))
+            .append(statement))
+
+def _build_journal(chart_of_accounts,
+                   journal_like,
+                   join_key='account_id'):
+
+    return (journal_like
+            .merge(right=chart_of_accounts,
+                   how='left',
+                   left_on=join_key,
+                   right_on='account_id' if join_key == 'account_id' else 'account_name',
+                   validate='many_to_one',
+                   suffixes=('', '_coa'))
+            .assign(net_amount=lambda df: np.where(_net_amount_predicate(df), df['amount'], df['amount'] * -1))
             .assign(debit_amount=lambda df: np.where((df['action'] == 'debit'), df['amount'], 0))
             .assign(credit_amount=lambda df: np.where((df['action'] == 'credit'), df['amount'], 0))
-            .pipe(lambda df: shape_like_journal(df)))
+            .pipe(lambda df: df[_get_journal_columns(df)]))
 
-def shape_like_journal(df):
+def _net_amount_predicate(df):
+    # comparison to `True` is due to Pandas weirdness
+    return (((df['action'] == 'debit') & (df['debit_increases_balance'] == True)) |
+            ((df['action'] == 'credit') & (df['debit_increases_balance'] == False)))
 
-    journal_columns =\
-        (['type'] + [col for col in df.columns if 'category_' in str(col)] +
-         ['account_id', 'account_name', 'transaction_id', 'date', 'description',
-          'net_amount', 'debit_amount', 'credit_amount', 'action'])
+def _journal_to_statement(journal_like, period_range):
+    return (journal_like
+            .assign(period=lambda df: df['date'].dt.to_period(period_range.freqstr))
+            .reset_index()
+            .pipe(lambda df: df[_get_statement_columns(df)]))
 
-    return df.pipe(lambda df: df[journal_columns])
+def _get_journal_columns(journal_like):
+    return (['type'] + [col for col in journal_like.columns if 'category_' in str(col)] +
+            ['account_id', 'account_name', 'transaction_id', 'date', 'description',
+             'net_amount', 'debit_amount', 'credit_amount', 'action'])
 
-def shape_like_statement(df):
-
-    journal_columns =\
-        (['period', 'type'] + [col for col in df.columns if 'category_' in str(col)] +
-         ['account_id', 'account_name', 'transaction_id', 'date', 'description',
-          'net_amount', 'debit_amount', 'credit_amount', 'action'])
-
-    return df.pipe(lambda df: df[journal_columns])
-
-def statement_index(stmt_data):
-    return (['period', 'type'] +
-            [c for c in stmt_data.columns if 'category_' in str(c)] +
-            ['account_name'])
-
-# return a dataframe that appends the cartesian product of
-# [periods x accounts] to the master journal with zero values
-#
-# this explicitly creates and zero-fills rows for any (account, period)
-# pairs that are not present in the journal in order to guarantee
-# that all are shown in downstream reports
-def statement_data(chart_of_accounts, journal, period_range):
-
-    freq = period_range.freqstr
-
-    pr_as_df = (period_range
-                .to_frame(name='period')
-                # use cj_key to fake a cross-join
-                .assign(cj_key=1))
-
-    filler_data = (chart_of_accounts
-                   .assign(cj_key=1)
-                   .merge(right=pr_as_df, on='cj_key')
-                   .drop('cj_key', 1)
-                   .rename(columns={'id': 'account_id', 'name': 'account_name'})
-                   .assign(date=lambda df: pd.Series(np.repeat(np.nan, len(df))).astype('datetime64[ns]'))
-                   .assign(credit_amount=0.0)
-                   .assign(debit_amount=0.0)
-                   .assign(net_amount=0.0)
-                   .assign(description='')
-                   .assign(action=None)
-                   .assign(transaction_id=DUMMY_TRANSACTION_ID)
-                   .pipe(lambda df: shape_like_statement(df)))
-
-    return (filler_data
-            .append(other=journal, sort=False)
-            .assign(period=lambda df: np.where(df['period'].isnull(), df['date'].dt.to_period(freq), df['period']))
-            .astype({'period': f'period[{freq}]', 'transaction_id': 'Int64'})
-            .reset_index(drop=True)
-            .fillna({'credit_amount': 0, 'debit_amount': 0, 'net_amount': 0})
-            .pipe(lambda df: shape_like_statement(df)))
+def _get_statement_columns(statement_like):
+    return (['period', 'type'] + [col for col in statement_like.columns if 'category_' in str(col)] +
+            ['account_id', 'account_name', 'transaction_id', 'date', 'description',
+             'net_amount', 'debit_amount', 'credit_amount', 'action'])
 
 ###############################################################################
-#### Report data #################################################################
+#### Report data ##############################################################
 ###############################################################################
 
-# account-wise representation of journal entries
-def general_ledger(journal):
-    grouped = journal.groupby('account_name')
+def general_ledger(stmt_data):
+    grouped = stmt_data.groupby('account_name')
     by_account = {k: grouped.get_group(k) for k in grouped.groups}
 
     output = {}
@@ -145,30 +141,27 @@ def general_ledger(journal):
     return output
 
 
-# sum of ledger entry net amounts for a given account within a given period
 def cash_flow(stmt_data, period_range):
     sd = stmt_data[(stmt_data['period'] >= period_range[0]) &
                    (stmt_data['period'] <= period_range[-1])]
 
-    report_index = statement_index(sd)
+    report_index = _get_report_index(sd)
     return (sd[report_index + ['net_amount']]
             .set_index(report_index, drop=True)
             .groupby(report_index)
             .sum())
 
-# cumulative sum of ledger entry net amounts for a given account, from
-# the beginning of time until the end of a given period
-#
-# to calculate:
-# - group all journal entries by account
-# - for each account:
-#   - sort journal entries by date
-#   - compute cumulative sum
-#   - group by period
-#   - select the value of the cumulative sum for the last date in the period
 def balance_sheet(stmt_data, period_range):
+
+    # - group journal entries by account
+    # - for each account:
+    #   - sort journal entries by date
+    #   - compute cumulative sum
+    #   - group by period
+    #   - select the value of the cumulative sum for the last date in the period
+
     return (stmt_data[(stmt_data['period'] <= period_range[-1])]
-            .pipe(lambda df: df.set_index(statement_index(df)))
+            .pipe(lambda df: df.set_index(_get_report_index(df)))
             .sort_values(['period', 'date'])
             .groupby('account_id')
             .apply(lambda df: (df.assign(bs_amount=df['net_amount'])
@@ -179,69 +172,92 @@ def balance_sheet(stmt_data, period_range):
             .reset_index()
             .pipe(lambda df: df[(df['period'] >= period_range[0]) &
                                 (df['period'] <= period_range[-1])])
-            .pipe(lambda df: df.set_index(statement_index(df))))
+            .pipe(lambda df: df.set_index(_get_report_index(df))))
+
+def _get_report_index(stmt_data):
+    return (['period', 'type'] +
+            [col for col in stmt_data.columns if 'category_' in str(col)] +
+            ['account_name'])
 
 ###############################################################################
-#### Calcs #################################################################
+#### Generated entries ########################################################
 ###############################################################################
 
-def infer_gains(coa, mj, stmt_data, balance_data_raw, period_range):
+def _generate_inferred_gains(chart_of_accounts,
+                             stmt_data,
+                             raw_balance_data,
+                             report_range):
+    balance_data =\
+        (raw_balance_data
+         .assign(period=lambda df: df['date'].dt.to_period(freq=report_range.freqstr))
+         .groupby(['account_name', 'period'])
+         .apply(lambda df: df.sort_values('date')[['date', 'balance']].head(1))
+         .droplevel(2))
 
-    bs = balance_sheet(stmt_data, period_range)
-    balance_diff =\
-        (balance_data_raw
+    # generate gains entries starting at the earliest available balance date -
+    # ignore the incoming `report_range` and use it only as a sotpping point
+    period_range = pd.period_range(balance_data['date'].sort_values()[0],
+                                   report_range[-1].end_time,
+                                   freq=report_range.freqstr)
+
+    journal_template =\
+        (balance_data
          .groupby('account_name')
          .apply(lambda df: df.reset_index()
-                .set_index('period')
-                .reindex(period_range)
-                .rename_axis('period'))
-         .pipe(lambda df: df.loc[df.index.get_level_values('period') >= period_range[0]])
-         .join(bs, how='right')
-         .pipe(lambda df: df.replace({col: {0: np.nan} for col in df.columns if col != 'account_id'}))
+                             .set_index('period')
+                             .reindex(period_range)
+                             .rename_axis('period'))
+         .join(balance_sheet(stmt_data, period_range),
+               how='right')
          .drop(columns=['account_name'])
          .groupby(level='account_name')
          .apply(lambda df: (df.assign(diff_vs_actual=lambda df: df['balance'] - df['bs_amount'])
                               .assign(diff_vs_actual_interp=lambda df: df['diff_vs_actual'].interpolate(method='linear'))
-                              .assign(gain=lambda df: df['diff_vs_actual_interp'].diff()))))
+                              .assign(gain=lambda df: df['diff_vs_actual_interp'].diff())))
+         .reset_index()
+         .assign(date=lambda df: df['period'].dt.to_timestamp())
+         .assign(transaction_id=DUMMY_TRANSACTION_ID))
 
-    journal_template = (balance_diff
-                        .reset_index()
-                        .assign(date=lambda df: df['period'].dt.to_timestamp())
-                        .assign(transaction_id=DUMMY_TRANSACTION_ID))
-
+    # TODO: verify signed-ness of gain/loss entries for liability accounts. ... maybe write some tests? :)
     gain_entries = [
-            journal_template
-                # TODO: does this hold for liability accounts as well?
-                .assign(action=lambda df: np.where(df['gain'] >= 0, 'debit', 'credit'))
-                .assign(amount=lambda df: df['gain'].abs())
-                .assign(description='Unrealized gain calculated from account statement'),
-            journal_template
-                .assign(description=lambda df: 'Unrealized gain calculated from rec data for: ' + df['account_name'])
-                .assign(account_name='unrealized gains')
-                # TODO: does this hold for liability accounts as well?
-                .assign(action=lambda df: np.where((df['gain'] * -1) >= 0, 'debit', 'credit'))
-                .assign(amount=lambda df: df['gain'].abs())
+        journal_template
+            .assign(action=lambda df: np.where(df['gain'] >= 0, 'debit', 'credit'))
+            .assign(amount=lambda df: df['gain'].abs())
+            .assign(description='Unrealized gain calculated from account statement'),
+        journal_template
+            .assign(description=lambda df: 'Unrealized gain calculated from rec data for: ' + df['account_name'])
+            .assign(account_name='unrealized gains')
+            .assign(action=lambda df: np.where((df['gain'] * -1) >= 0, 'debit', 'credit'))
+            .assign(amount=lambda df: df['gain'].abs())
     ]
 
-    mj_with_gains = (pd.concat(gain_entries)
-                     .dropna(subset=['amount'])
-                     .reset_index()
-                     .drop(columns=['type', 'category_0', 'category_1', 'account_id'])
-                     .pipe(lambda df: enrich_journal(coa, df, join_key='account_name'))
-                     .pipe(lambda df: pd.concat([mj, df])))
+    return (pd.concat(gain_entries)
+            .dropna(subset=['amount'])
+            .reset_index()
+            .drop(columns=['type', 'category_0', 'category_1', 'account_id'])
+            .pipe(lambda df: _build_journal(chart_of_accounts, df, join_key='account_name'))
+            .pipe(lambda df: df[_get_journal_columns(df)]))
 
-    return mj_with_gains, balance_diff
-
-def _diff_first_balance_vs_bs(df):
-    df = df.reset_index().replace({'bs_amount': {0: np.nan},
-                                   'balance': {0: np.nan}})
-
-    df['balance'] = df['balance'].interpolate(method='linear')
-    i_bal = x if (x := df['balance'].first_valid_index()) else 0
-    i_bs = x if (x := df['bs_amount'].first_valid_index()) else 0
-    idx = max(i_bal, i_bs)
-    diff = df['balance'].iat[idx] - df['bs_amount'].iat[idx]
-
-    return df.assign(bs_diff_vs_bal=diff).dropna().head(1)
-
+def _generate_closing_entries(chart_of_accounts, stmt_data, period_range):
+    return (chart_of_accounts
+            .pipe(lambda df: df.loc[df['closing_account'].notna(), ['account_name', 'closing_account']])
+            .assign(crossjoin_key=1)
+            .merge(right=period_range.to_frame(name='period').assign(crossjoin_key=1),
+                   on='crossjoin_key')
+            .drop('crossjoin_key', 1)
+            .set_index(['period', 'account_name'])
+            .merge(right=(stmt_data.set_index(['period', 'account_name'])
+                          .sort_index()
+                          .rename_axis(['period', 'account_name'])
+                          .dropna(subset=['net_amount'])),
+                   how='inner',
+                   on=['period', 'account_name'])
+            .pipe(lambda df: df[df['net_amount'] != 0])
+            .reset_index()
+            .pipe(lambda df: df[['period', 'closing_account', 'date', 'description', 'net_amount', 'action']])
+            .rename(columns={'closing_account': 'account_name', 'net_amount': 'amount'})
+            .assign(action=lambda df: np.where(df['action'] == 'debit', 'credit', 'debit'))
+            .assign(amount=lambda df: df['amount'] * -1)
+            .assign(transaction_id=DUMMY_TRANSACTION_ID)
+            .pipe(lambda df: _build_journal(chart_of_accounts, df, join_key='account_name')))
 
--- workbook.py
+++ workbook.py
@@ -9,73 +9,29 @@ import pandas as pd
 def run_workbook(data_dir):
     _set_pandas_display_options()
 
-    basic_reports(data_dir)
-    bank_rec_example(data_dir)
-
-def bank_rec_example(data_dir):
-
-    start_date = pd.Timestamp('20171101')
-    end_date = pd.Timestamp('20201231')
+    start_date = pd.Timestamp('20171231')
+    end_date = pd.Timestamp('20191231')
     period_range = pd.period_range(start=start_date, end=end_date, freq='M')
 
-    balance_data_raw = sb.data.balance_data(data_dir, period_range)
-    coa = sb.data.chart_of_accounts(data_dir)
-    mj = sb.data.master_journal(data_dir, coa)
-    sd = sb.data.statement_data(coa, mj, period_range)
+    coa = sb.data.fetch_chart_of_accounts(data_dir)
+    mj = sb.data.fetch_master_journal(data_dir, coa)
+    bd = sb.data.fetch_balance_data(data_dir)
+    sd = sb.data.statement_data(coa, mj, period_range, with_gains=True, balance_data=bd)
+
+    gl = sb.data.general_ledger(sd)
+    cf = sb.data.cash_flow(sd, period_range)
     bs = sb.data.balance_sheet(sd, period_range)
-
-    report = (bs.reset_index()
-              .pipe(lambda df: df[df['type'] == 'asset'])
-              .set_index(['account_name', 'period'])
-              .join(balance_data_raw[['balance']], how='left')
-              .assign(DIFF=lambda df: df['balance'] - df['bs_amount'])
-              .reset_index()
-              .set_index(['account_name', 'period'])
-              .sort_index()
-              .pipe(lambda df: df[['balance', 'bs_amount', 'DIFF']])
-              .rename_axis('values', axis=1)
-              .stack('values')
-              .unstack('account_name')
-              .unstack('values'))
-
-    print(report)
-
-def basic_reports(data_dir):
-    start_date = pd.Timestamp('20170101')
-    end_date = pd.Timestamp('20201231')
-    period_range = pd.period_range(start=start_date, end=end_date, freq='M')
-
-    coa = sb.data.chart_of_accounts(data_dir)
-    mj = sb.data.master_journal(data_dir, coa)
-    sd = sb.data.statement_data(coa, mj, period_range)
-    bd = sb.data.balance_data(data_dir, period_range)
-
-    mj_with_gains, balance_diff = sb.data.infer_gains(coa, mj, sd, bd, period_range)
-    sd_with_gains = sb.data.statement_data(coa, mj_with_gains, period_range)
-
-    gl = sb.data.general_ledger(mj_with_gains)
-    cf = sb.data.cash_flow(sd_with_gains, period_range)
-
-    bs = sb.data.balance_sheet(sd_with_gains, period_range)
-
-    gains = (bs.loc[bs.index.get_level_values('type') == 'asset', :]
-             .reset_index()
-             .set_index(['period', 'account_name'])
-             .rename_axis('values', axis=1)
-             .stack('values')
-             .unstack('account_name')
-             .unstack('values'))
 
     glr = sb.reports.general_ledger(gl)
     cfr = sb.reports.cash_flow(cf)
     bsr = sb.reports.balance_sheet(bs)
 
     print(f'''
-        {cfr}
+    {glr}
 
-        {bsr}
+    {cfr}
 
-        {gains}
+    {bsr}
      ''')
 
 def _set_pandas_display_options():
@@ -86,5 +42,6 @@ def _set_pandas_display_options():
     pd.options.display.max_colwidth = 50
     pd.options.display.max_info_columns = 10000
     pd.options.display.max_info_rows = 10000
-    pd.options.display.max_rows = 10000
+    pd.options.display.max_rows = 10000000
     pd.options.display.max_seq_items = 10000
+
